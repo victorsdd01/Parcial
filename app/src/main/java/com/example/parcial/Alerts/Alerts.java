8d78834394ef755c1102a64a894c2431f83679f7
victorsdd01 <victormanuel.1517@hotmail.com>
2021-06-22 14:18:08 -0600

    clase crear un nuevo usuario validada...

--- app/src/main/java/com/example/parcial/Alerts/Alerts.java
+++ app/src/main/java/com/example/parcial/Alerts/Alerts.java
@@ -1,8 +1,38 @@
 package com.example.parcial.Alerts;
 
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.widget.Toast;
+
+import com.example.parcial.CrearNuevoUsuario;
+import com.example.parcial.Login;
+
 public class Alerts {
+
+    Activity activity;
+
+    public  Alerts(Activity activity)
+     {
+         this.activity=activity;
+     }
 
     //En esta clase estan todas las ventanitas emergentes "alertas" usen POO para invocarlas...
 
+   public void AlertaUsuario()
+    {
+        AlertDialog.Builder alerta= new AlertDialog.Builder(activity);
+        alerta.setMessage("Usuario creado correctamente!!!")
+                .setCancelable(false)
+                .setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        activity.startActivity(new Intent(activity,Login.class));
+                    }
+                })
+                .show();
 
+
+    } //llace del metodo...
 }
--- app/src/main/java/com/example/parcial/CrearNuevoUsuario.java
+++ app/src/main/java/com/example/parcial/CrearNuevoUsuario.java
@@ -1,14 +1,62 @@
 package com.example.parcial;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class CrearNuevoUsuario extends AppCompatActivity {
-
+    EditText usuario,password,vpassword,email;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crear_nuevo_usuario);
+
+        iniciarController();
     }
-}
+
+    private void iniciarController()
+      {
+          usuario=(EditText)findViewById(R.id.CrearNu_edtxUsuario);
+          password=(EditText)findViewById(R.id.CrearNu_edtxPassword);
+          vpassword=(EditText)findViewById(R.id.CrearNu_edtxVerificarPassword);
+          email=(EditText)findViewById(R.id.CrearNu_edtxEmail);
+      }
+
+      public void CrearNuevoUsuario(View view)
+       {
+         String usuario,pasword,vpassword,email;
+
+         usuario=this.usuario.getText().toString();
+         pasword=this.password.getText().toString();
+         vpassword=this.vpassword.getText().toString();
+         email=this.email.getText().toString();
+
+         if(!usuario.isEmpty())
+           {
+              if(!pasword.isEmpty())
+                {
+                    if(vpassword.equals(pasword))
+                      {
+                          if(!email.isEmpty())
+                            {
+                                // aqui se hace la insercion en la BD
+                            }else
+                                {
+                                   this.email.setError("Este campo es requerido");
+                                }
+                      }else
+                          {
+                              this.vpassword.setError("Las contraseñas no coinciden");
+                          }
+                }else
+                    {
+                      this.password.setError("Este campo es requerido");
+                    }
+           }else
+               {
+                 this.usuario.setError("Este campo es requerido");
+               }
+       }// llave del metodo...
+
+}// llave de la clase...
