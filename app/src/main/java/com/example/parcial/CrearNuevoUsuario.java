6ed5e402c8e5f735b1ea92e9c2e957c1b1f06e67
victorsdd01 <victormanuel.1517@hotmail.com>
2021-06-22 13:31:55 -0600

    activity crear nuevo usuario creada y validada

---
+++ app/src/main/java/com/example/parcial/CrearNuevoUsuario.java
@@ -0,0 +1,14 @@
+package com.example.parcial;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CrearNuevoUsuario extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crear_nuevo_usuario);
+    }
+}
--- app/src/main/java/com/example/parcial/Login.java
+++ app/src/main/java/com/example/parcial/Login.java
@@ -2,13 +2,59 @@ package com.example.parcial;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Login extends AppCompatActivity {
+     EditText usuario,password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        iniciarController();
     }
+
+    private void iniciarController()
+     {
+         usuario=(EditText)findViewById(R.id.edtx_usuario);
+         password=(EditText)findViewById(R.id.edtx_password);
+
+     }
+
+     // metodo que valida que exista un usuario...
+     public  void IniciarSesion(View view)
+      {
+          String usuario,password;
+          //aqui se almacena lo que el usuario escribe en los inputs...
+          usuario=this.usuario.getText().toString();
+          password=this.password.getText().toString();
+
+          if(!usuario.isEmpty())
+            {
+                if(!password.isEmpty())
+                  {
+                      //aqui se va a la otra activity
+                  }else
+                      {
+
+                      }
+
+            } else
+                  {
+
+                  }
+      }
+
+      // metodo para registrar un nuevo usuario
+
+    public void CrearNuevoUsuario(View view)
+     {
+         startActivity(new Intent(getApplicationContext(),CrearNuevoUsuario.class));
+     }
+
+
 }
