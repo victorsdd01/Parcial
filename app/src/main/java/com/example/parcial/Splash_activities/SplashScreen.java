1d24c4375d949d7188187a781b6410805aed6f9c
victorsdd <victormanuel.1517@hotmail.com>
2021-06-29 13:48:09 -0600

    victor
    change SplashScreen location

--- app/src/main/java/com/example/parcial/Splash_activities/SplashScreen.java
+++ app/src/main/java/com/example/parcial/Splash_activities/SplashScreen.java
@@ -1,11 +1,13 @@
-package com.example.parcial;
+package com.example.parcial.Splash_activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.view.WindowManager;
+
+import com.example.parcial.Login;
+import com.example.parcial.R;
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -23,7 +25,7 @@ public class SplashScreen extends AppCompatActivity {
          new Handler().postDelayed(new Runnable() {
              @Override
              public void run() {
-                 startActivity(new Intent(SplashScreen.this,Login.class));
+                 startActivity(new Intent(SplashScreen.this, Login.class));
              }
          },5000);
      }
